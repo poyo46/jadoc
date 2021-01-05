813e44728c57abc48c28e63cbb8e8498b7107ca1
poyo46 <poyo4rock@gmail.com>
2021-01-05 17:16:54 +0900

    v0.1.2

--- jadoc/__init__.py
+++ jadoc/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "jadoc"
 __description__ = "Tokenizes Japanese documents to enable CRUD operations."
 __url__ = "https://github.com/poyo46/jadoc"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "poyo46"
 __author_email__ = "poyo4rock@gmail.com"
 __license__ = "Apache-2.0"
